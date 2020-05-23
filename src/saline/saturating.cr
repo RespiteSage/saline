71410a48bdee2c2f825e646fbbba35f7f87d0064
Benjamin Wade <benderrwade@gmail.com>
2020-05-23 17:14:28 -0400

    Put Saturating in the Saline module
    Honestly, I hadn't realized this wasn't already the case.

--- src/saline/saturating.cr
+++ src/saline/saturating.cr
@@ -1,104 +1,106 @@
-# A `Saturating` `Number` clamps to its maximum or minimum values instead of
-# overflowing.
-#
-# This wrapper type changes overflow behavior to saturate at the contained
-# `Number` type's minimum and maximum. The code snippet below demonstrates
-# this concept with an Int32.
-#
-# ```crystal
-# n = Saturating(Int32).new(Int32::MAX)
-# n += 1 # => 2147483647 (Int32::MAX)
-# n = Saturating(Int32).new(Int32::MIN)
-# n -= 1 # => -2147483648 (Int32::MIN)
-# ```
-#
-# Note: This class will only work with `Number` types. In order to avoid
-#       unexpected behavior, `T` is checked at compile-time to determine
-#       whether it is a `Number` type.
-struct Saturating(T)
-  include Comparable(T)
-  include Comparable(Saturating(T))
+module Saline
+  # A `Saturating` `Number` clamps to its maximum or minimum values instead of
+  # overflowing.
+  #
+  # This wrapper type changes overflow behavior to saturate at the contained
+  # `Number` type's minimum and maximum. The code snippet below demonstrates
+  # this concept with an Int32.
+  #
+  # ```crystal
+  # n = Saturating(Int32).new(Int32::MAX)
+  # n += 1 # => 2147483647 (Int32::MAX)
+  # n = Saturating(Int32).new(Int32::MIN)
+  # n -= 1 # => -2147483648 (Int32::MIN)
+  # ```
+  #
+  # Note: This class will only work with `Number` types. In order to avoid
+  #       unexpected behavior, `T` is checked at compile-time to determine
+  #       whether it is a `Number` type.
+  struct Saturating(T)
+    include Comparable(T)
+    include Comparable(Saturating(T))
 
-  # The `Number` value represented by this `Saturating(T)`
-  getter value : T
+    # The `Number` value represented by this `Saturating(T)`
+    getter value : T
 
-  # Create a new `Saturating(T)` with the given *value*.
-  def initialize(@value : T)
-    {% unless T < Number %}
+    # Create a new `Saturating(T)` with the given *value*.
+    def initialize(@value : T)
+      {% unless T < Number %}
       {% raise "The generic type of Saturating must be a Number type!" %}
     {% end %}
-  end
-
-  # Returns the result of adding `self` and *other*.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def +(other : Number) : Saturating(T)
-    if other < 0
-      self - (-other)
-    else
-      begin
-        new_value = value + other
-        Saturating(T).new(new_value)
-      rescue OverflowError
-        Saturating(T).new(T::MAX)
-      end
     end
-  end
 
-  # Returns the result of subtracting *other* from `self`.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def -(other : Number) : Saturating(T)
-    if other < 0
-      self + (-other)
-    else
-      begin
-        new_value = value - other
-        Saturating(T).new(new_value)
-      rescue OverflowError
-        Saturating(T).new(T::MIN)
-      end
-    end
-  end
-
-  # Returns the result of multiplying `self` and *other*.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def *(other : Number) : Saturating(T)
-    begin
-      new_value = value * other
-      Saturating.new(new_value)
-    rescue OverflowError
-      if (value < 0) ^ (other < 0) # this does cancelling of negative signs
-        Saturating(T).new(T::MIN)
+    # Returns the result of adding `self` and *other*.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def +(other : Number) : Saturating(T)
+      if other < 0
+        self - (-other)
       else
-        Saturating(T).new(T::MAX)
+        begin
+          new_value = value + other
+          Saturating(T).new(new_value)
+        rescue OverflowError
+          Saturating(T).new(T::MAX)
+        end
       end
     end
-  end
 
-  # Returns the result of adding `self` and *other*.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def +(other : Saturating(T)) : Saturating(T)
-    self + other.value
-  end
+    # Returns the result of subtracting *other* from `self`.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def -(other : Number) : Saturating(T)
+      if other < 0
+        self + (-other)
+      else
+        begin
+          new_value = value - other
+          Saturating(T).new(new_value)
+        rescue OverflowError
+          Saturating(T).new(T::MIN)
+        end
+      end
+    end
 
-  # Returns the result of subtracting *other* from `self`.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def -(other : Saturating(T)) : Saturating(T)
-    self - other.value
-  end
+    # Returns the result of multiplying `self` and *other*.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def *(other : Number) : Saturating(T)
+      begin
+        new_value = value * other
+        Saturating.new(new_value)
+      rescue OverflowError
+        if (value < 0) ^ (other < 0) # this does cancelling of negative signs
+          Saturating(T).new(T::MIN)
+        else
+          Saturating(T).new(T::MAX)
+        end
+      end
+    end
 
-  # Returns the result of multiplying `self` and *other*.
-  # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
-  def *(other : Saturating(T)) : Saturating(T)
-    self * other.value
-  end
+    # Returns the result of adding `self` and *other*.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def +(other : Saturating(T)) : Saturating(T)
+      self + other.value
+    end
 
-  def <=>(other : Saturating(T))
-    self.value <=> other.value
-  end
+    # Returns the result of subtracting *other* from `self`.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def -(other : Saturating(T)) : Saturating(T)
+      self - other.value
+    end
 
-  def <=>(other : Number)
-    self.value <=> other
-  end
+    # Returns the result of multiplying `self` and *other*.
+    # Returns `T::MAX` or `T::MIN` (as appropriate) in case of overflow.
+    def *(other : Saturating(T)) : Saturating(T)
+      self * other.value
+    end
 
-  forward_missing_to value
+    def <=>(other : Saturating(T))
+      self.value <=> other.value
+    end
+
+    def <=>(other : Number)
+      self.value <=> other
+    end
+
+    forward_missing_to value
+  end
 end
