f526ec242ef19dc423fc66633db05d9c9ef7b01a
Benjamin Wade <benjamin@va.wagner.com>
2021-04-28 17:00:45 -0400

    Add benchmarks
    These should help with testing the magnitude of optimizations in future
    updates.

---
+++ benchmarks/asymmetric_saturating.cr
@@ -0,0 +1,49 @@
+require "benchmark"
+require "../src/saline.cr"
+
+include Saline
+
+fake_val = 0
+Benchmark.ips do |x|
+  {% begin %}
+    {% for first_type in {Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64} %}
+      {% for second_type in ([Int8, Int16, Int32, Int64, UInt8, UInt16, UInt32, UInt64].reject { |type| type == first_type }) %}
+        x.report("Saturating({{first_type}}) + Saturating({{second_type}}), no saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new(1)
+          val_{{second_type}} = Saturating({{second_type}}).new(2)
+          fake_val = val_{{first_type}} + val_{{second_type}}
+        end
+
+        x.report("Saturating({{first_type}}) - Saturating({{second_type}}), no saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new(2)
+          val_{{second_type}} = Saturating({{second_type}}).new(1)
+          fake_val = val_{{first_type}} - val_{{second_type}}
+        end
+
+        x.report("Saturating({{first_type}}) * Saturating({{second_type}}), no saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new(3)
+          val_{{second_type}} = Saturating({{second_type}}).new(2)
+          fake_val = val_{{first_type}} * val_{{second_type}}
+        end
+
+        x.report("Saturating({{first_type}}) + Saturating({{second_type}}), with saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new({{first_type}}::MAX - 2)
+          val_{{second_type}} = Saturating({{second_type}}).new({{second_type}}::MAX - 1)
+          fake_val = val_{{first_type}} + val_{{second_type}}
+        end
+
+        x.report("Saturating({{first_type}}) - Saturating({{second_type}}), with saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new({{first_type}}::MIN + 1)
+          val_{{second_type}} = Saturating({{second_type}}).new(2)
+          fake_val = val_{{first_type}} - val_{{second_type}}
+        end
+
+        x.report("Saturating({{first_type}}) * Saturating({{second_type}}), with saturation") do
+          val_{{first_type}} = Saturating({{first_type}}).new({{first_type}}::MAX - 2)
+          val_{{second_type}} = Saturating({{second_type}}).new({{second_type}}::MAX - 1)
+          fake_val = val_{{first_type}} * val_{{second_type}}
+        end
+      {% end %}
+    {% end %}
+  {% end %}
+end
